8841f2516ea1843dbcc185491cf7e9e6889ce6ca
Nouman Chohan <chohannouman95@gmail.com>
2023-02-08 23:46:16 -0500

    5 commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
+import * as dotenv from "dotenv";
 import registerRouter from "./Register/RegisterRoutes.js";
 import DbConnection from "./DbConnection/DbConnection.js";
 import loginRouter from "./Login/LoginRoutes.js";
 import productRouter from "./Product/ProductRoutes.js";
 import cartProductsRoutes from "./CartProducts/CartProductsRoutes.js";
-import * as dotenv from "dotenv";
+import cors from "cors";
+
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 DbConnection;
 app.use("/", registerRouter);
